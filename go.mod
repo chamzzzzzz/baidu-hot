2aa9b2e865fe3eb3c4b2a88388ece9d92a152646
Liu Chen <chamzzzzzz@gmail.com>
2022-05-25 00:34:16 +0800

    archiving

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/chamzzzzzz/baidu-hot
 
 go 1.18
 
-require github.com/anaskhan96/soup v1.2.5
+require (
+	github.com/anaskhan96/soup v1.2.5
+	github.com/mattn/go-sqlite3 v1.14.13
+)
 
 require (
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/anaskhan96/soup"
+	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
@@ -13,6 +16,7 @@ import (
 type Hot struct {
 	Title   string
 	Summary string
+	Date    string
 }
 
 type Crawler struct {
@@ -74,37 +78,199 @@ func (c *Crawler) SaveToFile(filePath string) error {
 	return os.WriteFile(filePath, []byte(content), 0666)
 }
 
-func main() {
+type Archive struct {
+	Source      string
+	Count       int
+	IgnoreCount int
+}
+
+type Archiver struct {
+	db         *sql.DB
+	selectStmt *sql.Stmt
+	insertStmt *sql.Stmt
+	Archives   []*Archive
+}
+
+func (a *Archiver) Archiving() error {
+	if err := a.prepare(); err != nil {
+		return err
+	}
+
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		return err
+	}
+
+	var fileNames []string
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+	sort.Strings(fileNames)
+
+	tx, err := a.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	for _, fileName := range fileNames {
+		if !strings.HasSuffix(fileName, ".hot.txt") {
+			continue
+		}
+
+		date := strings.ReplaceAll(fileName, ".hot.txt", "")
+		if _, err := time.Parse("2006-01-02-15-04-05", date); err != nil {
+			return err
+		}
+
+		data, err := os.ReadFile(fileName)
+		if err != nil {
+			return err
+		}
+
+		lines := strings.Split(string(data), "\n")
+		count := len(lines) / 2
+		ignoreCount := 0
+		for i := 0; i < count; i++ {
+			hot := &Hot{
+				Title:   lines[i*2],
+				Summary: lines[i*2+1],
+				Date:    date,
+			}
+
+			if err, ignore := a.insert(tx, hot); err != nil {
+				return err
+			} else if ignore {
+				ignoreCount++
+			}
+		}
+
+		archive := &Archive{}
+		archive.Source = fileName
+		archive.Count = count
+		archive.IgnoreCount = ignoreCount
+		a.Archives = append(a.Archives, archive)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Archiver) prepare() error {
+	db, err := sql.Open("sqlite3", "hot.db")
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS hot (date TEXT, title TEXT, summary TEXT);")
+	if err != nil {
+		return err
+	}
+
+	selectStmt, err := db.Prepare("SELECT date, title FROM hot WHERE title = ?")
+	if err != nil {
+		return err
+	}
+
+	insertStmt, err := db.Prepare("INSERT INTO hot(date, title, summary) VALUES(?,?,?)")
+	if err != nil {
+		return err
+	}
+
+	a.db = db
+	a.selectStmt = selectStmt
+	a.insertStmt = insertStmt
+	return nil
+}
+
+func (a *Archiver) insert(tx *sql.Tx, hot *Hot) (error, bool) {
+	rows, err := tx.Stmt(a.selectStmt).Query(hot.Title)
+	if err != nil {
+		return err, false
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var date, title string
+		err := rows.Scan(&date, &title)
+		if err != nil {
+			return err, false
+		}
+
+		firstTime, err := time.Parse("2006-01-02-15-04-05", date)
+		if err != nil {
+			return err, false
+		}
+
+		latestTime, err := time.Parse("2006-01-02-15-04-05", hot.Date)
+		if err != nil {
+			return err, false
+		}
+
+		duration := latestTime.Sub(firstTime)
+		if duration < 24*7*time.Hour {
+			return nil, true
+		}
+	}
+
+	if _, err := tx.Stmt(a.insertStmt).Exec(hot.Date, hot.Title, hot.Summary); err != nil {
+		return err, false
+	}
+
+	return nil, false
+}
+
+func archiving() int {
+	archiver := &Archiver{}
+
+	if err := archiver.Archiving(); err != nil {
+		fmt.Println("archiving error:", err)
+		return 1
+	}
+
+	for _, archive := range archiver.Archives {
+		fmt.Printf("archive %s %d/%d/%d\n", archive.Source, archive.Count-archive.IgnoreCount, archive.IgnoreCount, archive.Count)
+	}
+	fmt.Println("archiving finished")
+	return 0
+}
+
+func crawling() int {
 	crawler := &Crawler{}
 
-	retry := 0
-	for {
-		err := crawler.Crawling()
-		if err == nil {
-			break
-		}
+	if err := crawler.Crawling(); err != nil {
 		fmt.Println("crawling error:", err)
-
-		if retry >= 10 {
-			fmt.Printf("crawling failed after %d retry!\n", retry)
-			os.Exit(1)
-		}
-
-		retry++
-		fmt.Printf("crawling retry after %ds.\n", retry)
-		time.Sleep(time.Duration(retry) * time.Second)
+		return 1
 	}
 
 	loc, err := time.LoadLocation("Asia/Chongqing")
 	if err != nil {
 		fmt.Println("load location error:", err)
-		os.Exit(1)
+		return 1
 	}
 
 	now := time.Now().In(loc)
 	filePath := fmt.Sprintf("%d-%02d-%02d-%02d-%02d-%02d.hot.txt", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 	if err := crawler.SaveToFile(filePath); err != nil {
 		fmt.Println("save to file error:", err)
-		os.Exit(1)
+		return 1
 	}
+	fmt.Println("crawling finished")
+	return 0
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		os.Exit(crawling())
+	}
+
+	if os.Args[1] == "archive" {
+		os.Exit(archiving())
+	}
+
+	fmt.Println("invalid args!")
+	os.Exit(1)
 }
